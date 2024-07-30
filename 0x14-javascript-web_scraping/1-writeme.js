f3ed5e85b9a94571311dc370b02b3a37bac0bb10
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-07-30 20:43:17 +0300

    checks

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,18 +2,20 @@
 
 const fs = require('fs');
 
-// Check if the file path is provided
-if (process.argv.length < 3) {
-  console.error('Usage: ./readFile.js <file-path>');
+// Check if the correct number of arguments is provided
+if (process.argv.length < 4) {
+  console.error('Usage: ./writeToFile.js <file-path> <string-to-write>');
   process.exit(1);
 }
 
 const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
+// Write the string to the file using UTF-8 encoding
+fs.writeFile(filePath, stringToWrite + '\n', 'utf8', (err) => {
   if (err) {
     console.error(err);
     return;
   }
-  console.log(data);
 });
+
