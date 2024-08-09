c7f305e3a7534185dbf97cb11dfd37a75889c38f
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-08-09 21:00:47 +0300

    printing

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// Script that prints a string
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,2 @@
+0x12. JavaScript - Warm up: Introduction to JavaScript and getting familiar
+with JavaScript syntax
