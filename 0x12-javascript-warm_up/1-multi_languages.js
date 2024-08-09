efb846df7e6f32d880d22f51726fbeabffcd379c
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-08-09 21:12:57 +0300

    3_lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script that prints three lines
+let myVar = 'C is fun';
+console.log(myVar);
+myVar = 'Python is cool';
+console.log(myVar);
+myVar = 'JavaScript is amazing';
+console.log(myVar);
