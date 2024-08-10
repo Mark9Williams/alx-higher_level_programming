5425302f747a21147befa016b7e8f9e2746ab8b3
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-08-10 10:40:45 +0300

    looping

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// a script that prints 3 lines but by using an array of string and a loop
+
+const messages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < messages.length; i++) {
+  console.log(messages[i]);
+}
