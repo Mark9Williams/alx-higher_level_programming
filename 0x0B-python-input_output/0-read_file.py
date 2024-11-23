868f771c32dfd80db3c9b462652ee3f8f286328d
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-11-23 16:53:26 +0000

    checks

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,6 +5,4 @@
 def read_file(file_name="", encoding="utf-8"):
     """reads a text file and prints it to stdout"""
     with open(file_name) as f:
-        for line in f:
-            print(line, end="")
-    print("\n", end="")
+        print(f.read(), end="")
