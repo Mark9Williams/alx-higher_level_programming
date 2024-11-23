732cfe6b0868ebba9bcd8f3aac7a7d4a260635cf
Mark9Williams <mawejjemarkwilliam@gmail.com>
2024-11-23 17:13:10 +0000

    some_checks

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -4,6 +4,7 @@
 
 class Student:
     """class that defines student"""
+
     def __init__(self, first_name, last_name, age):
         """instantiation method"""
         self.first_name = first_name
@@ -13,5 +14,9 @@ class Student:
     def to_json(self, attrs=None):
         """Retrieves a dictionary representation of a student"""
         if isinstance(attrs, list) and all(isinstance(i, str) for i in attrs):
-            return {key: value for key, value in self.__dict__.items() if key in attrs}
+            return {
+                key: value
+                for key, value in self.__dict__.items()
+                if key in attrs
+            }
         return self.__dict__
